2d699f6cb44659ef1386f10c001013fffcd7aa89
corazonnn <zero7364@icloud.com>
2023-04-20 09:25:22 +0900

    Wi-Fi接続とNTPクライアントを設定して時刻を取得

--- RFID_DoorKey.ino
+++ RFID_DoorKey.ino
@@ -1,23 +1,25 @@
-#include <WiFi.h>
-#include <WiFiAP.h>
-#include <WiFiClient.h>
-#include <WiFiGeneric.h>
-#include <WiFiMulti.h>
-#include <WiFiSTA.h>
-#include <WiFiScan.h>
-#include <WiFiServer.h>
-#include <WiFiType.h>
-#include <WiFiUdp.h>
-
 #include <M5Stack.h>
+#include <WiFi.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
 #include "MFRC522_I2C.h"
 
+// WiFi接続情報
+// const char *ssid     = "Buffalo-2G-47c0";
+// const char *password = "7ksad38v5svea";
+const char *ssid     = "elecom2g-f297fb";
+const char *password = "97pn7752rjdj";
+
+// NTPクライアントの設定
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "pool.ntp.org", 9 * 3600, 60000);
+
+//RFIDタグの初期化
 MFRC522 mfrc522(0x28);
-    unsigned long tagDetectionDuration = 0;
-    bool isMessage = false; //今日の通知を送ったかどうか
+unsigned long tagDetectionDuration = 0;//ドアが開いている時間（RFIDが検出されていない時間）
+bool isMessage = false; //今日の通知を送ったかどうか
 
 void setup() {
-  //セットアップ
     Serial.begin(9600);
     M5.begin();   //M5stackオブジェクトの初期化         
     M5.Power.begin();
@@ -25,7 +27,24 @@ void setup() {
     M5.Lcd.setTextColor(WHITE);
     M5.lcd.setTextSize(2);  
     Wire.begin();  
+    
+    //Wifi
+    M5.Lcd.println("Connecting to Wi-Fi...");
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      M5.Lcd.print(".");
+    }
 
+    M5.Lcd.println("\nWi-Fi connected.");
+    M5.Lcd.println("Getting time...");
+
+    timeClient.begin();
+    timeClient.update();
+
+
+
+    //RFID
     mfrc522.PCD_Init(); //RFIDの初期化
     M5.Lcd.setCursor(0, 0); //描画位置の設定
     M5.Lcd.fillScreen(BLACK); //背景色の設定
@@ -33,9 +52,14 @@ void setup() {
 }
 
 void loop() {
+    M5.Lcd.setCursor(0, 0);
+    M5.Lcd.fillScreen(BLACK);
 
-    M5.Lcd.setCursor(40, 47);
-    M5.Lcd.fillRect(42, 47, 320, 20, BLACK); //Please put the cardを邪魔しないため
+    //NTPサーバーから時刻取得のため更新
+    GetTime();
+    
+    // M5.Lcd.setCursor(40, 47);
+    // M5.Lcd.fillRect(42, 47, 320, 20, BLACK); //Please put the cardを邪魔しないため
 
     //1日一回だけ実行、sendMessage送ったらもう実行しない。日付が変わればまた
     if(!isMessage){
@@ -64,4 +88,13 @@ void loop() {
 void sendNotification() {
   //slackへの通知
   //isMessage = true;
+}
+
+void GetTime(){
+timeClient.update();
+    M5.Lcd.println("Current time:");
+    String formattedTime = timeClient.getFormattedTime();
+    M5.Lcd.println(formattedTime);
+    M5.Lcd.println("");
+    //delay(1000);
 }
