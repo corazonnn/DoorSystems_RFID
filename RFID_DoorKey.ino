2daf8c071ba002775af27912579c08da65fbfbd2
corazonnn <zero7364@icloud.com>
2023-04-22 14:29:28 +0900

    リファクタリング

--- RFID_DoorKey.ino
+++ RFID_DoorKey.ino
@@ -4,14 +4,14 @@
 #include "MFRC522_I2C.h"
 #include <HTTPClient.h>
 
-
 // WiFi接続情報
 // const char *ssid     = "Buffalo-2G-47C0";
 // const char *password = "7ksad38v5svea";
 const char *ssid     = "elecom2g-f297fb";
 const char *password = "97pn7752rjdj";
-const char *webhookUrl = "https://hooks.slack.com/services/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";
 
+//slack WebhookURL
+const char *webhookUrl = "";
 
 // NTPクライアントの設定
 WiFiUDP ntpUDP;
@@ -20,10 +20,14 @@ NTPClient timeClient(ntpUDP, "pool.ntp.org", 9 * 3600, 60000);
 //RFIDタグの初期化
 MFRC522 mfrc522(0x28);
 
-unsigned long tagDetectionDuration = 0;//ドアが開いている時間（RFIDが検出されていない時間）
+//ドアが開いている時間（＝RFIDが検出されていない時間）
+unsigned long tagDetectionDuration = 0;
 
-bool isMessage = false; //今日の通知を送ったかどうか
+//今日の通知を送ったかどうか
+bool isMessage = false; 
 
+
+//slackへ通知を送る
 void postMessage(String message) {
   if (WiFi.status() == WL_CONNECTED) {
     HTTPClient http;
@@ -53,11 +57,9 @@ void GetTime(){
     String formattedTime = timeClient.getFormattedTime();//format: 13:47:40
     M5.Lcd.println(formattedTime);
     M5.Lcd.println("");
-
-    
 }
 
-//
+//日付を監視し、isMessageを更新する
 void updateFlag(){
     timeClient.update();
 
@@ -74,65 +76,61 @@ void updateFlag(){
 
     //本番（0時になったらisMessageを更新）
     // if(currentHour == 0){
-    //   isMessage = true;
+    //   isMessage = false;
     // }
 
     //テスト（0秒になったら(1分ごと)isMessageを更新）
     if(currentSeconds == 0){
       isMessage = false;
-      lastDay = currentDay;
     }
 }
 
 void setup() {
-    //共通の設定
+    //共通のセットアップ
     Serial.begin(9600);
     M5.begin();   //M5stackオブジェクトの初期化         
     M5.Power.begin();
     M5.Lcd.begin();
-    M5.Lcd.setTextColor(WHITE);
-    M5.lcd.setTextSize(2);  
+    M5.Lcd.setTextColor(WHITE); //テキストカラー
+    M5.lcd.setTextSize(2);  //テキストサイズ
+    M5.Lcd.setCursor(0, 0); //描画位置の設定
+    M5.Lcd.fillScreen(BLACK); //背景色の設定
     Wire.begin();  
     
-    //Wifi
+    //Wifiのセットアップ
     M5.Lcd.println("Connecting to Wi-Fi...");
     WiFi.begin(ssid, password);
     while (WiFi.status() != WL_CONNECTED) {
       delay(500);
       M5.Lcd.print(".");
     }
-
     M5.Lcd.println("\nWi-Fi connected.");
     M5.Lcd.println("Getting time...");
 
+    //timeClientのセットアップ
     timeClient.begin();
     timeClient.update();
 
-
-
-    //RFID
-    mfrc522.PCD_Init(); //RFIDの初期化
-    M5.Lcd.setCursor(0, 0); //描画位置の設定
-    M5.Lcd.fillScreen(BLACK); //背景色の設定
-    M5.Lcd.println("Please put the card");
+    //RFIDのセットアップ
+    mfrc522.PCD_Init();    
 }
 
 void loop() {
     M5.Lcd.setCursor(0, 0);
     M5.Lcd.fillScreen(BLACK);
 
-    //NTPサーバーから時刻取得のため更新
+    //NTPサーバーから現在の時刻を取得し、出力
     GetTime();
 
-    //1日一回だけ実行、sendMessage送ったらもう実行しない。日付が変わればまた
+    //1日一回だけ、最初のドアの施錠を検知し、slackへ通知する
     if(!isMessage){
-      if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) {//タグが検出できている
+      if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) {//タグ検出OK
         tagDetectionDuration =0;
-      } else {//タグが認識できていない＝ドアが開いている可能性
+      } else {//タグ検出NG（＝ドアが開いている可能性）
         tagDetectionDuration += 50;
       }
 
-      if (tagDetectionDuration >= 150) { // タグが1000ms以上検出できない場合、ドアが開いているとする
+      if (tagDetectionDuration >= 150) { // タグが一定時間検出できない場合、ドアが開いているとする（Durationを設けないと、OpenedとClosedが交互に出力されてしまうため）
         M5.Lcd.println("Door Opened");
         postMessage("key");
         isMessage = true;
@@ -141,7 +139,7 @@ void loop() {
       }
       delay(500);
     }else{
-      //次の日になったらisMessageを更新する
+      //日付の更新とともにisMessageを更新
       updateFlag();
       delay(1000);
       return;
