1aa7185a356a8a55461980492f5c77992441c99d
corazonnn <zero7364@icloud.com>
2023-05-11 17:36:48 +0900

    鍵の持ち出しを管理。３種類のボタンの入力に応じてslackにメッセージを送信

--- RFID_DoorKey.ino
+++ RFID_DoorKey.ino
@@ -4,14 +4,15 @@
 #include "MFRC522_I2C.h"
 #include <HTTPClient.h>
 
+
 // WiFi接続情報
-// const char *ssid     = "Buffalo-2G-47C0";
-// const char *password = "7ksad38v5svea";
-const char *ssid     = "elecom2g-f297fb";
-const char *password = "97pn7752rjdj";
+const char *ssid     = "Buffalo-2G-47C0";
+const char *password = "7ksad38v5svea";
+// const char *ssid     = "elecom2g-f297fb";
+// const char *password = "97pn7752rjdj";
 
 //slack WebhookURL
-const char *webhookUrl = "";
+const char *webhookUrl = "............................";
 
 // NTPクライアントの設定
 WiFiUDP ntpUDP;
@@ -64,25 +65,24 @@ void updateFlag(){
     timeClient.update();
 
     //時間と日付の取得
-    int currentDay = timeClient.getDay();
     int currentHour = timeClient.getHours();
     int currentSeconds = timeClient.getSeconds();//テスト用
 
-    //m5stackに出力
-    M5.Lcd.print("Hour: ");
-    M5.Lcd.println(currentHour);
-    M5.Lcd.print("Seconds: ");
-    M5.Lcd.println(currentSeconds);
+    //m5stackに「時間」「秒」出力
+    // M5.Lcd.print("Hour: ");
+    // M5.Lcd.println(currentHour);
+    // M5.Lcd.print("Seconds: ");
+    // M5.Lcd.println(currentSeconds);
 
     //本番（0時になったらisMessageを更新）
-    // if(currentHour == 0){
-    //   isMessage = false;
-    // }
-
-    //テスト（0秒になったら(1分ごと)isMessageを更新）
-    if(currentSeconds == 0){
+    if(currentHour == 0){
       isMessage = false;
     }
+
+    //テスト（0秒になったら(1分ごと)isMessageを更新）
+    // if(currentSeconds == 0){
+    //   isMessage = false;
+    // }
 }
 
 void setup() {
@@ -95,7 +95,7 @@ void setup() {
     M5.lcd.setTextSize(2);  //テキストサイズ
     M5.Lcd.setCursor(0, 0); //描画位置の設定
     M5.Lcd.fillScreen(BLACK); //背景色の設定
-    Wire.begin();  
+    Wire.begin();
     
     //Wifiのセットアップ
     M5.Lcd.println("Connecting to Wi-Fi...");
@@ -119,6 +119,8 @@ void loop() {
     M5.Lcd.setCursor(0, 0);
     M5.Lcd.fillScreen(BLACK);
 
+    M5.update();//ボタン状態更新
+
     //NTPサーバーから現在の時刻を取得し、出力
     GetTime();
 
@@ -141,6 +143,42 @@ void loop() {
     }else{
       //日付の更新とともにisMessageを更新
       updateFlag();
+
+      //ここで画面UI作りたいな
+      //本体ボタン項目表示
+      M5.Lcd.drawRect(37, 216, 66, 23, WHITE);    // 本体ボタン表示枠用四角
+      M5.Lcd.drawRect(127, 216, 66, 23, WHITE);
+      M5.Lcd.drawRect(217, 216, 66, 23, WHITE);
+
+      //日本語表示できるならやりたかったやつ
+      // M5.Lcd.setTextFont(1.5);
+      // M5.Lcd.setCursor(0, 50);
+      // M5.Lcd.setTextColor(ORANGE);
+      // M5.Lcd.println("When leaving the lab with the key, press one of the following buttons");
+      
+      M5.Lcd.setTextFont(1);                                // フォント
+      M5.Lcd.setTextColor(WHITE);
+      M5.Lcd.setCursor(55, 221);    M5.Lcd.print("18F");   // アナログ出力電圧アップボタン
+      M5.Lcd.setCursor(150, 221);   M5.Lcd.print("2F"); // アナログ出力電圧ダウンボタン 135
+      M5.Lcd.setCursor(230, 221);   M5.Lcd.print("21F"); // ON/OFFボタン
+      
+      M5.Lcd.setTextColor(ORANGE);
+      M5.Lcd.setCursor(203, 200);   M5.Lcd.print("came back"); // ON/OFFボタン
+      M5.Lcd.setTextColor(WHITE);
+
+      //鍵の持ち出しを管理する
+      M5.Lcd.setCursor(0, 110);
+      if(M5.BtnA.wasPressed()){//18階に鍵を持ち出し
+        M5.Lcd.println("Button was Pressed!!!!!");
+        postMessage("18th floor");
+      }else if(M5.BtnB.wasPressed()){//2階に鍵を持ち出し
+        M5.Lcd.println("Button was Pressed!!!!!");
+        postMessage("2nd floor");
+      }else if(M5.BtnC.wasPressed()){//21階に鍵を返却
+        M5.Lcd.println("Button was Pressed!!!!!");
+        postMessage("21st floor");
+      }
+
       delay(1000);
       return;
     }
